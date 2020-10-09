c911df447fcb56bce14ccf101e1fa6ae25619b8a
Ivan <ninja79@github.com>
2020-10-09 13:44:13 +0200

    Working version of API, Model + Controllers

--- Gemfile
+++ Gemfile
@@ -9,6 +9,10 @@ gem 'rails', '~> 6.0.3', '>= 6.0.3.3'
 gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
+
+# Fast json Serializer
+gem 'fast_jsonapi' 
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+airlines = Airline.create([
+
+  {
+  name: "United Airlines",
+  image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+  },
+  {
+  name: "Delta Airlines",
+  image_url: "https://open-flights.s3.amazonaws.com/Delta.png"
+  },
+  {
+  name: "Alaska Airlines",
+  image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png"
+  }
+])
+
+reviews = Review.create([
+  {
+  title: "Great airline",
+  description: "Lovely time onboard",
+  score: 5,
+  airline: airlines.first
+  },
+  {
+  title: "Bad airline",
+  description: "Awful service onboard",
+  score: 1,
+  airline: airlines.first
+  }
+])
