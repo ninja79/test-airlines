e164a647d21ed36bc82e8e0446380755d8ce44da
Ivan <ninja79@github.com>
2020-10-10 13:43:01 +0200

    Axios request is working

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
-  return <div>This is the Airlines#index view for app.</div>
+  const [airlines, setAirlines] = useState([])
+  
+  useEffect(() => {
+    // Get all airlines
+    // Update airlines in our state  
+
+    axios.get('/api/v1/airlines.json')
+      .then ( resp => console.log(resp) )
+      .catch( resp => console.log(resp) )    
+  }, [airlines.length])
+  
+  return (
+    <div>This is the Airlines#index view for app.</div>
+  )
 }
 
 export default Airlines
