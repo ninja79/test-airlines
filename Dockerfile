fdb27eb049ebcccfa57c13acbb0ea935e05092e3
Ivan <ninja79@github.com>
2020-10-19 19:33:21 +0200

    Main page starts working with React

--- Dockerfile
+++ Dockerfile
@@ -47,5 +47,5 @@ EXPOSE 9000
 CMD ["bundle", "exec", "rails", "server", "-b", "0.0.0.0", "-p", "9000"]
 
 #docker build -t airlines .
-#docker run -itP -v $(pwd):/app -p 9000:9000 airlines
+#docker run -itP -v $(pwd):/app -p 9000:9000 airlines /bin/bash
 #docker exec -it $(docker ps -q) /bin/bash
--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect, Fragment} from 'react'
 import axios from 'axios'
+import Airline from './Airline' 
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([])
@@ -9,12 +10,32 @@ const Airlines = () => {
     // Update airlines in our state  
 
     axios.get('/api/v1/airlines.json')
-      .then ( resp => console.log(resp) )
+      .then ( resp => {
+        //console.log(resp.data.length)
+        setAirlines (resp.data.data)      
+      })
       .catch( resp => console.log(resp) )    
   }, [airlines.length])
   
+  const grid = airlines.map( item => {
+    return (
+      <Airline 
+        key={item.attributes.name}
+        attributes={item.attributes}
+      />
+    )  
+  })  
+  
   return (
-    <div>This is the Airlines#index view for app.</div>
+    <div className="home">
+      <div className="header">
+        <h1>OpenFlights</h1>
+        <div className="subheader">Reviews for Airlines</div>
+      </div>
+      <div className="grid">      
+        {grid}
+      </div>
+    </div>
   )
 }
 
