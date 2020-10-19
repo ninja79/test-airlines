1fbce827a701517f4a008da5e35b5eaf3d89df09
Ivan <ninja79@github.com>
2020-10-19 20:02:16 +0200

    Grid is now displaying good

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,6 +1,35 @@
 import React, { useState, useEffect, Fragment} from 'react'
 import axios from 'axios'
 import Airline from './Airline' 
+import styled from 'styled-components'
+
+const Home = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const Header = styled.div`
+  padding: 100px 100px 10px 100px;
+  
+  h1 {
+    font-size: 42px;
+  }
+`
+
+const Subheader = styled.div`
+  font-weight: 300;
+  font-size: 26px;
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100;
+  padding: 20px;
+`
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([])
@@ -27,15 +56,15 @@ const Airlines = () => {
   })  
   
   return (
-    <div className="home">
-      <div className="header">
+    <Home>
+      <Header>
         <h1>OpenFlights</h1>
-        <div className="subheader">Reviews for Airlines</div>
-      </div>
-      <div className="grid">      
+        <Subheader>Reviews for Airlines</Subheader>
+      </Header>
+      <Grid>      
         {grid}
-      </div>
-    </div>
+      </Grid>
+    </Home>
   )
 }
 
